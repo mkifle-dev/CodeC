22763ccad0bb086adc119c2f1744ebd0c3cc422e
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-01-14 11:14:41 -0500

    codewar

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+// Write a function "greet" that returns "hello world!"
+
+//my answer
+function greet(){
+	return "hello world!"
+  }
+
+
+// Another Answer: 
+
+const greet = () => 'hello world!';
