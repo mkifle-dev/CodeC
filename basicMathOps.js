8b3538131e09c00bda6d320ae0ad7da6e8ea2c0d
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-10-01 22:04:52 -0400

    basic math

---
+++ basicMathOps.js
@@ -0,0 +1,58 @@
+// question is: 
+
+/*
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation
+
+function basicOp(operation, value1, value2)
+{
+  // Code
+}
+
+*/ 
+
+
+// My answer: 
+
+function basicOp(operation, value1, value2) {
+    if (operation === '+') {
+        return value1 + value2
+    }
+    if (operation === '-') {
+        return value1 - value2
+    }
+    if (operation === '*') {
+        return value1 * value2
+    }
+    if (operation === '/') {
+        return value1 / value2
+    }
+}
+
+
+//other ways:
+
+function basicOp(operation, value1, value2)
+{
+  return eval(value1 + operation + value2);
+}
+
+//other ways:
+const basicOperations = {
+	'+': (a,b) => a + b,
+	'-': (a,b) => a - b,
+	'*': (a,b) => a * b,
+	'/': (a,b) => a / b,
+  }
+
+
+//other way: 
+
+  function basicOp(operation, value1, value2) {
+	return  operation == '+' ? value1 + value2 :
+			operation == '-' ? value1 - value2 :
+			operation == '*' ? value1 * value2 :
+			operation == '/' ? value1 / value2 : 'Wrong Operation';
+  }
