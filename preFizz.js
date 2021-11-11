c480256eb3a02b99cb3334316f0de4f159772b3e
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-11-11 10:39:10 -0500

    codewar

---
+++ preFizz.js
@@ -0,0 +1,27 @@
+/* This is the first step to understanding FizzBuzz.
+
+Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+
+Your expected output is an array of positive integers from 1 to n (inclusive).
+
+Your job is to write an algorithm that gets you from the input to the output.
+
+*/ 
+
+// input = posative integer (n); n >= 1
+//  output = array of + int from 1-n (including n)
+// 
+
+function preFizz (n){
+	let newarr = []; 
+	for (let i=1; i<= n; i++){
+		newarr.push(i)
+	}
+	return newarr 
+}
+
+
+
+
+
+
