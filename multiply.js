e3588a6c3c50211b8382a96901088cc5097696cb
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-11-12 11:02:48 -0500

    codewar

---
+++ multiply.js
@@ -0,0 +1,26 @@
+/*
+
+This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+*/
+
+// My Answer
+
+function multiply(a, b){
+	return a*b
+   }
+
+
+// Other Answer
+
+const multiply = (a, b) => a * b;
+
+// Other Answer
+
+function multiply(a, b){
+	var prod = a * b;
+	return prod;
+  }x
