8ae9f0bf4255d1ee7a81e61411615143cf0f494b
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-11-15 10:13:17 -0500

    codewar

---
+++ square.js
@@ -0,0 +1,25 @@
+/*
+
+Now you have to write a function that takes an argument and returns the square of it.
+
+
+*/
+
+
+// My response
+
+var square = function(a){
+	return a*a;
+}
+
+// Answer one
+const square = a => a * a;
+
+
+
+
+// Another one
+function square(num){
+	var num = Math.pow(num, 2);
+	return num;
+  };
