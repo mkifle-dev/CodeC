0ac50e1742860572230965e0ca55b07365d4623d
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-12-08 10:46:44 -0500

    codewar

---
+++ oppNumbers.js
@@ -0,0 +1,13 @@
+//Very simple, given a number, find its opposite.
+
+function opposite(number) {
+	if (number > 0 ){
+		return -number
+		}else {
+	   return Math.abs(number)   // Math.abs conversts negative numbers into posative
+	}
+   }
+
+   function opposite(number) {
+	return -1 * number; 
+   }
