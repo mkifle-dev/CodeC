17158eaf63b6f49359fd4ca7a180a29b9d54c3c1
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2022-01-01 11:11:25 -0500

    codewar

---
+++ isItOdd.js
@@ -0,0 +1,32 @@
+
+/*
+Instructions:
+Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+//Solution:
+
+// Way 1
+function even_or_odd(number) {
+	if (number % 2 === 0 ){
+	  return "Even"
+	}else{
+	  return "Odd"
+	}
+  }
+  
+  // Way 2 
+  function even_or_odd(number) {
+	return number % 2 === 0 ? "Even": "Odd"
+  }
+
+  //paramenter
+  // 
+
+
+function even_or_odd(number){
+	return number % 2 === 0 ? "Even" : "Odd"; 
+}
+
+// another oneee
+
