1536f907120bb8f64d214f9b1578f80f7a9bdf0a
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2022-01-19 14:15:14 -0500

    codewar

---
+++ pickaset.js
@@ -0,0 +1,9 @@
+//  Write a function to get the first elements of asequence. Passing a parameter n (default=1) will return the first n elements of the sequence.
+
+// If n == 0 return an empty sequence []
+
+function first(arr, n) {
+    n === undefined ? n = 1 : n = n
+    return arr.slice(0, n)
+    
+}
