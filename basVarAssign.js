553580d2cfb398888c8ff38333f1d1d5cc8814f4
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2022-02-08 11:38:11 -0500

    codewar

---
+++ basVarAssign.js
@@ -0,0 +1,17 @@
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+/*
+
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+
+*/
+
+// answer one
+let name = "codewa.rs"; 
+
+//another one
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
